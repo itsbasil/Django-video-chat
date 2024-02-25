a95426f3bb311b36138558689997e3fd9d506f47
Basil George <itsbasilgeorge@gmail.com>
2024-02-26 01:46:13 +0530

    Display video source to page

--- base/views.py
+++ base/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from agora_token_builder import RtcTokenBuilder
+import random
+import time
 
 # Create your views here.
+
+def getToken(request):
+    appId = '49bade666b6242348dd4575b04bb0134'
+    appCertificate= '54b600edf3064bfe9a04c4722c35637a'
+    channelName = request.GET.get('channel')
+    uid = random.randint(1,230)
+    expirationTimeInSeconds = 3600 * 24
+    currentTimeStamp = time.time() 
+    privilegeExpiredTs = currentTimeStamp + expirationTimeInSeconds
+    role = 1
+
+    token = RtcTokenBuilder.buildTokenWithUid(appId, appCertificate, channelName, uid, role, privilegeExpiredTs)
+    return JsonResponse({'token':token,'uid':uid},safe=False)
+
 def lobby(request):
     return render(request, 'base/lobby.html')
 def room(request):
--- static/js/streams.js
+++ static/js/streams.js
@@ -1,4 +1,100 @@
 const APP_ID ='49bade666b6242348dd4575b04bb0134'
-const CHANNEL='main'
-const TOKEN='007eJxTYFjYl5WsfdpKMtG6w05pp1Ju5PTEGZe2TPNeUBq4MMlqjoUCg4llUmJKqpmZWZKZkYmRsYlFSoqJqblpkoFJUpKBobFJ3osbqQ2BjAxCmxUZGRkgEMRnYchNzMxjYAAAqwsdPQ=='
+const CHANNEL= sessionStorage.getItem('room')
+const TOKEN= sessionStorage.getItem('token')
+let UID = Number(sessionStorage.getItem('UID'))
 
+const client = AgoraRTC.createClient({mode:'rtc', codec:'vp8'})
+
+let localTracks =[]
+let remoteUsers ={}
+
+let joinAndDisplayLocalStream = async () => {
+
+ document.getElementById('room-name').innerText = CHANNEL
+
+ client.on('user-published', handleUserJoined)
+ client.on('user-left', handleUserLeft)
+
+ try{
+ await client.join(APP_ID, CHANNEL, TOKEN, UID)
+}catch(error){
+        console.error(error)
+        window.open('/','_self')
+}
+
+   localTracks = await AgoraRTC.createMicrophoneAndCameraTracks()
+
+   let player =`<div class="video-container" id="user-container-${UID}">
+                <div class="username-wrapper"><span class="user-name">My Name</span></div>
+                 <div class="video-player" id="user-${UID}"></div>
+                </div>`
+        document.getElementById('video-streams').insertAdjacentHTML('beforeend',player)
+        
+        localTracks[1].play(`user-${UID}`)
+
+        await client.publish([localTracks[0], localTracks[1]])
+}
+  
+
+let handleUserJoined =async (user, mediaType) =>  {
+        remoteUsers[user.uid]  = user
+        await client.subscribe(user,mediaType)
+
+        if(mediaType === 'video'){
+                let player= document.getElementById(`user-container-${user.uid}`)
+                 if(player !=null){
+                        player.remove()
+                 }
+                 player =`<div class="video-container" id="user-container-${user.uid}">
+                <div class="username-wrapper"><span class="user-name">My Name</span></div>
+                 <div class="video-player" id="user-${user.uid}"></div>
+                </div>`
+        document.getElementById('video-streams').insertAdjacentHTML('beforeend',player)
+     user.videoTrack.play(`user-${user.uid}`)     
+}
+    if(mediaType === 'audio'){
+        user.audioTrack.play()
+
+
+    }
+}
+
+let handleUserLeft = async (user) => {
+        delete remoteUsers[user.uid]
+        document.getElementById(`user-container-${user.uid}`).remove()
+}
+
+let leaveAndRemoveLocalStream = async () =>{
+       for(let i=0; localTracks.length > i; i++){
+        localTracks[i].stop()
+        localTracks[i].close() 
+       } 
+       await client.leave()
+       window.open('/','_self')
+}
+
+let toggleCamera = async () => {
+        if(localTracks[1].muted){
+                await localTracks[1].setMuted(false)
+                e.target.style.backgroundColor = '#fff'
+        }else{
+                await localTracks[1].setMuted(true)
+                e.target.style.backgroundColor = `rgb(75, 95, 172)`
+        }
+}
+
+let toggleMic = async () => {
+        if(localTracks[0].muted){
+                await localTracks[0].setMuted(false)
+                e.target.style.backgroundColor = '#fff'
+        }else{
+                await localTracks[0].setMuted(true)
+                e.target.style.backgroundColor = `rgb(75, 95, 172)`
+        }
+}
+
+joinAndDisplayLocalStream()
+
+document.getElementById('leave-btn').addEventListener('click', leaveAndRemoveLocalStream)
+document.getElementById('camera-btn').addEventListener('click', toggleCamera)
+document.getElementById('mic-btn').addEventListener('click', toggleMic)
